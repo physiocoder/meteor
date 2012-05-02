276b221e5bddcdb51370932219add558d1b52e45
David Greenspan <dgreenspan@alum.mit.edu>
2012-05-02 16:41:50 -0700

    invoke jade parser

--- packages/jade/package.js
+++ packages/jade/package.js
@@ -2,3 +2,23 @@ Package.describe({
   summary: "Jade template engine"
 });
 
+var jade = require('jade');
+var fs = require('fs');
+
+Package.register_extension(
+  "jade", function(bundle, source_path, serve_path, where) {
+    serve_path = serve_path + '.js';
+
+    var source = fs.readFileSync(source_path, 'utf8');
+
+    var parser = new jade.Parser(source, source_path);
+    var parsed = JSON.stringify(parser.parse());
+
+    bundle.add_resource({
+      type: 'js',
+      path: serve_path,
+      data: new Buffer('J='+parsed+';'),
+      where: where
+    });
+  }
+);
