45da03ed20ad384b65febe118567ff878725eb3d
Chris Mather <mather.chris@gmail.com>
2013-03-24 14:41:55 -0700

    reactive-vision package example application.

---
+++ examples/unfinished/reactive-vision/vision.js
@@ -0,0 +1,46 @@
+Items = new Meteor.Collection("items");
+
+if (Meteor.isServer) {
+  function seed () {
+    Items.remove({});
+
+    for (var i = 0; i < 10; i++) {
+      Items.insert({title: "Item " + i});
+    }
+  }
+
+  Meteor.startup(seed);
+
+  Meteor.publish("items", function () {
+    return Items.find();
+  });
+}
+
+if (Meteor.isClient) {
+  Meteor.subscribe("items");
+
+  Template.main.helpers({
+    title: function () {
+      return Session.get("title") || "Not Set";
+    }
+  });
+
+  Template.list.helpers({
+    items: function () {
+      return Items.find();
+    }
+  });
+
+  Template.textTemplate.helpers({
+    text: function () {
+      return Session.get("text") || "Text";
+    }
+  });
+
+  Meteor.startup(function () {
+    Meteor.ReactiveVision.onLog(function (msg) {
+      $('<li>').text(msg).appendTo('#logger');
+    });
+  });
+}
+
